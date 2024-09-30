efe2616a4619d368178a69c4912f22f52f53555e
Chutinut Jirasritana-anan <chutinut@Chutinuts-MacBook-Pro.local>
2024-09-30 16:50:12 +0700

    :bulb:updated: comment in variable package

--- internal-package/1-variables/variables.go
+++ internal-package/1-variables/variables.go
@@ -20,11 +20,11 @@ func ShowVariables() {
 	fmt.Println("Pi value is : ", pi)
 }
 
-/**
+/*
 Variable type (name : param)
 1. string : string // text variable  (Eg. "ABC", "1", "")
 2. boolean : bool // boolean variable (Eg. true, false)
 3. number : int(8, 16, 32, 64) // number integer variable (Eg. 1, 50, 100, 999, -1, -5) *int(value bit size)
 4. number : uint // number integer variable greater than or equal 0 (Eg. 1, 50, 100, 999)
 5. number : float // number integer variable greater than or equal 0 (Eg. 1.2, 3.414, -0.25) *float(value bit size)
-**/
+*/
